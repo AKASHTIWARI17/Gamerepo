790d982ff514ab2b320652e0ebb41306edc2d033
Akash Tiwari <akashti821032@gmail.com>
2024-07-25 17:53:45 +0530

    push

--- src/Components/NavbarForBack.jsx
+++ src/Components/NavbarForBack.jsx
@@ -12,7 +12,7 @@ const NavbarForBack = () => {
   return (
     <nav className="bg-white fixed w-full rounded-md z-20 top-0 py-4 px-6 sm:ml-10 sm:m sm:px-8 md:px-10 lg:px-12 xl:px-16 flex items-center justify-between h-14 shadow-md">
       <div className="flex items-center space-x-4">
-        <FontAwesomeIcon icon={faArrowLeft} className="text-lg sm:text-xl md:text-2xl" />
+        <FontAwesomeIcon icon={faArrowLeft} className="text-lg sm:text-xl md:text-2xl"/>
       </div>
       <div className="text-center font-medium text-sm sm:text-base md:text-lg">
         <h2>Ultimate Mini Golf Universe</h2>
