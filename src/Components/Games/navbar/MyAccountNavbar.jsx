5c970e18657cdd5331ff495371afd76c853e72d5
Akash Tiwari <akashti821032@gmail.com>
2024-07-26 20:28:51 +0530

    GamePreview

--- src/Components/Games/navbar/MyAccountNavbar.jsx
+++ src/Components/Games/navbar/MyAccountNavbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const MyAccountNavbar = () => {
   return (
     <>
-      <nav className="bg-black fixed w-full top-0  px-[80px] justify-between  md:h-20 flex items-center">
+      <nav className="bg-black text-white  fixed w-full top-0  px-[80px] justify-between  md:h-20 flex items-center">
         <div className=" header text-4xl  font-sans font-bold flex items-center  justify-center">
           <Link to="/">
             {" "}
--- src/Components/NavbarForBack.jsx
+++ src/Components/NavbarForBack.jsx
@@ -7,7 +7,7 @@ const NavbarForBack = ({ name }) => {
   const moveBacks = useMoveBack();
 
   return (
-    <nav className="bg-white fixed w-full rounded-md z-20 top-0 py-4 px-6 sm:ml-10 sm:m sm:px-8 md:px-10 lg:px-12 xl:px-16 flex items-center justify-between h-14 shadow-md">
+    <nav className="bg-black text-white fixed w-full rounded-md z-20 top-0 py-4 px-6 sm:ml-10 sm:m sm:px-8 md:px-10 lg:px-12 xl:px-16 flex items-center justify-between h-14 shadow-md">
       <div className="flex items-center space-x-4">
         <FontAwesomeIcon
           onClick={moveBacks}
